2d9665b45e8259a82d054b071da7250e1d6cf548
NoKlapeer <noklapeer@tsn.at>
2023-01-29 21:09:03 +0100

    final push

--- 5.Klasse/verkettete_Liste/main.py
+++ 5.Klasse/verkettete_Liste/main.py
@@ -16,6 +16,24 @@ class LinkedList:
             self.tail.next = new_item
             self.tail = new_item
         self.length += 1
+    
+    def find_index(self, value):
+        current = self.head
+        index = 0
+        while current:
+            if current.value == value:
+                return index
+            index += 1
+            current = current.next
+        return None
+    
+    def __getitem__(self, index):
+        if index >= self.length or index < 0:
+            return None
+        current = self.head
+        for i in range(index):
+            current = current.next
+        return current.value
 
     def __len__(self):
         return self.length
@@ -28,6 +46,7 @@ class LinkedList:
             current = current.next
         return value_str
 
+
 class Item:
     def __init__(self, value):
         self.value = value
@@ -35,9 +54,13 @@ class Item:
 
 
 if __name__ == "__main__":
-    list = LinkedList()
+    mlist = LinkedList()
     for _ in range(10):
-        list.add_last(random.randint(1,100))
-    list.add_last(20)
-    print(list)
-    print(len(list))
+        mlist.add_last(random.randint(1,100))
+    mlist.add_last(20)
+    print("Liste ausgeben:", mlist)
+    search_value = int(input("Gib eine Zahl zum Finden des Indexes ein: "))
+    print("Index von", search_value, "ist", mlist.find_index(search_value))
+    search_index = int(input("Gib einen Index zum Finden der Zahl ein: "))
+    print("Index von", search_index, "ist", mlist[search_index])
+    print("Laenge der Liste:", len(mlist))
