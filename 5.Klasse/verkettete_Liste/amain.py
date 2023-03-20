4cadc706658c8afb409c53860345f8353aca1496
NoKlapeer <noklapeer@tsn.at>
2023-03-20 16:12:14 +0100

    arraylist hopefully final

--- 5.Klasse/verkettete_Liste/amain.py
+++ 5.Klasse/verkettete_Liste/amain.py
@@ -2,25 +2,38 @@ import random
 
 class ArrayList:
     def __init__(self):
-        self.data = []
+        self.platz = 10
+        self.len = 0
+        self.data = [None] * self.platz
 
-    def length(self):
-        return len(self.data)
-
-    def add(self, item):
-        self.data.append(item)
+    def add(self, element):
+        self.mache_platz()
+        self.data[self.len] = element
+        self.len += 1
 
     def remove(self, index):
-        del self.data[index]
+        for i in range(index, self.len-1):
+            self.data[i] = self.data[i+1]
+        self.data[self.len-1] = None
+        self.len -= 1
 
     def __getitem__(self, index):
         return self.data[index]
 
-    def index_of(self, item):
-        return self.data.index(item)
-    
+    def length(self):
+        return self.len
+
+    def mache_platz(self):
+        if self.len == self.platz:
+            neuer_platz = self.platz * 2
+            neue_data = [None] * neuer_platz
+            for i in range(self.len):
+                neue_data[i] = self.data[i]
+            self.data = neue_data
+            self.platz = neuer_platz
+
     def __str__(self):
-        return str(self.data)
+        return str(self.data[:self.len])
 
 if __name__ == "__main__":
     arraylist = ArrayList()
@@ -28,12 +41,10 @@ if __name__ == "__main__":
         arraylist.add(random.randint(1, 100))
     print("Länge von Arraylist:", arraylist.length())
     print("Liste ausgeben:", arraylist)
-    search_value = int(input("Gib eine Zahl zum Finden des Indexes ein: "))
-    print("Index von", search_value, "ist", arraylist.index_of(search_value))
-    search_index = int(input("Gib einen Index zum Finden der Zahl ein: "))
-    print("Index von", search_index, "ist", arraylist[search_index])
-    print("Index 2 wird gelöscht ...")
-    arraylist.remove(2)
+    such_index = int(input("Gib einen Index zum Finden der Zahl ein: "))
+    print("Index von", such_index, "ist", arraylist[such_index])
+    entf_index = int(input("Welcher Index soll gelöscht werden? "))
+    arraylist.remove(entf_index)
     print("Liste ausgeben:", arraylist)
 
     
